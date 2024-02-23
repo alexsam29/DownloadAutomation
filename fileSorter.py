d604363c4c11a38a22aeefd99b5c3293510c8725
Alexander Samaniego <69481177+alexsam29@users.noreply.github.com>
2024-02-23 03:45:56 -0500

    Formatting and documentation.

--- fileSorter.py
+++ fileSorter.py
@@ -7,28 +7,84 @@ from shutil import move
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
-# File paths
+# Directories
 source_dir = "C:/Users/alex_/Downloads"
 dest_dir_audio = "C:/Users/alex_/Downloads/Audio"
 dest_dir_video = "C:/Users/alex_/Downloads/Video"
 dest_dir_image = "C:/Users/alex_/Downloads/Images"
 dest_dir_documents = "C:/Users/alex_/Downloads/Documents"
 
-# Supported image types
-image_exts = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd",
-              ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt",
-              ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
-# Supported Video types
-video_exts = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg", ".mp4", ".mp4v", ".m4v", ".avi", ".wmv",
-              ".mov", ".qt", ".flv", ".swf", ".avchd"]
-# Supported Audio types
+# Image, video, audio, and document extensions
+image_exts = [
+    ".jpg",
+    ".jpeg",
+    ".jpe",
+    ".jif",
+    ".jfif",
+    ".jfi",
+    ".png",
+    ".gif",
+    ".webp",
+    ".tiff",
+    ".tif",
+    ".psd",
+    ".raw",
+    ".arw",
+    ".cr2",
+    ".nrw",
+    ".k25",
+    ".bmp",
+    ".dib",
+    ".heif",
+    ".heic",
+    ".ind",
+    ".indd",
+    ".indt",
+    ".jp2",
+    ".j2k",
+    ".jpf",
+    ".jpf",
+    ".jpx",
+    ".jpm",
+    ".mj2",
+    ".svg",
+    ".svgz",
+    ".ai",
+    ".eps",
+    ".ico",
+]
+video_exts = [
+    ".webm",
+    ".mpg",
+    ".mp2",
+    ".mpeg",
+    ".mpe",
+    ".mpv",
+    ".ogg",
+    ".mp4",
+    ".mp4v",
+    ".m4v",
+    ".avi",
+    ".wmv",
+    ".mov",
+    ".qt",
+    ".flv",
+    ".swf",
+    ".avchd",
+]
 audio_exts = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
-# Supported Document types
 document_exts = [".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
 
 
 class MoveHandler(FileSystemEventHandler):
+    """A class that handles file movements based on their extensions."""
+
     def on_modified(self, event):
+        """Event handler for file modification event.
+
+        Args:
+            event (FileSystemEvent): The event object.
+        """
         with scandir(source_dir) as files:
             for file in files:
                 name = file.name
@@ -38,36 +94,75 @@ class MoveHandler(FileSystemEventHandler):
                 self.search_document_files(file, name)
 
     def search_audio_files(self, file, name):
+        """Searches for audio files and moves them to the destination directory.
+
+        Args:
+            file (DirEntry): The file object.
+            name (str): The name of the file.
+        """
         for audio_ext in audio_exts:
             if name.endswith(audio_ext) or name.endswith(audio_ext.upper()):
                 move_file(dest_dir_audio, file, name)
-                logging.info(f"Audio file, {name} moved into folder path: {dest_dir_audio}")
+                logging.info(
+                    f"Audio file, {name} moved into folder path: {dest_dir_audio}"
+                )
 
     def search_video_files(self, file, name):
+        """Searches for video files and moves them to the destination directory.
+
+        Args:
+            file (DirEntry): The file object.
+            name (str): The name of the file.
+        """
         for video_ext in video_exts:
             if name.endswith(video_ext) or name.endswith(video_ext.upper()):
                 move_file(dest_dir_video, file, name)
-                logging.info(f"Video file, {name} moved into folder path: {dest_dir_video}")
+                logging.info(
+                    f"Video file, {name} moved into folder path: {dest_dir_video}"
+                )
 
     def search_image_files(self, file, name):
+        """Searches for image files and moves them to the destination directory.
+
+        Args:
+            file (DirEntry): The file object.
+            name (str): The name of the file.
+        """
         for image_ext in image_exts:
             if name.endswith(image_ext) or name.endswith(image_ext.upper()):
                 move_file(dest_dir_image, file, name)
-                logging.info(f"Image file, {name} moved into folder path: {dest_dir_image}")
+                logging.info(
+                    f"Image file, {name} moved into folder path: {dest_dir_image}"
+                )
 
     def search_document_files(self, file, name):
+        """Searches for document files and moves them to the destination directory.
+
+        Args:
+            file (DirEntry): The file object.
+            name (str): The name of the file.
+        """
         for document_ext in document_exts:
             if name.endswith(document_ext) or name.endswith(document_ext.upper()):
                 move_file(dest_dir_documents, file, name)
-                logging.info(f"Document file, {name} moved into folder path: {dest_dir_documents}")
+                logging.info(
+                    f"Document file, {name} moved into folder path: {dest_dir_documents}"
+                )
 
 
 def move_file(dest, file, name):
-    # Create directory if not exists
+    """Moves a file to the specified destination directory.
+
+    If a file with the same name already exists in the destination directory, a unique name is generated.
+
+    Args:
+        dest (str): The destination directory.
+        file (DirEntry): The file object.
+        name (str): The name of the file.
+    """
     if not exists(dest):
         makedirs(dest)
 
-    # If file exists, add unique name
     if exists(f"{dest}/{name}"):
         unique_name = create_unique(dest, name)
         prevName = join(dest, name)
@@ -76,8 +171,16 @@ def move_file(dest, file, name):
     move(file, dest)
 
 
-# Create unique name if file already exists
 def create_unique(dest, name):
+    """Creates a unique name for a file by appending a number to the filename.
+
+    Args:
+        dest (str): The destination directory.
+        name (str): The name of the file.
+
+    Returns:
+        str: The unique name for the file.
+    """
     filename, ext = splitext(name)
     file_number = 1
 
@@ -88,11 +191,13 @@ def create_unique(dest, name):
     return name
 
 
-# Monitors the current directory recursively
+# Monitors the source directory recursively for file movements
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
     path = source_dir
     event_handler = MoveHandler()
     observer = Observer()
